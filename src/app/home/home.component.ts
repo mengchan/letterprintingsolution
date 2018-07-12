40ad96158624ddeec525df4d61d7631fba3418d9
Mengchan Chham <339356@PRU.intranet.asia>
2018-07-12 16:55:18 +0700

    push letter part form company pc

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,25 +18,14 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
       this.tableData1 = {
-          headerRow: [ 'ID', 'Name', 'Country', 'City', 'Salary'],
+          headerRow: [ 'ID', 'Policy Number', 'letter Type', 'Letter Status', 'Status','Stamp Status','Comment','Preview','APE','Agent Number'],
           dataRows: [
-              ['1', 'Dakota Rice', 'Niger', 'Oud-Turnhout', '$36,738'],
-              ['2', 'Minerva Hooper', 'Curaçao', 'Sinaai-Waas', '$23,789'],
-              ['3', 'Sage Rodriguez', 'Netherlands', 'Baileux', '$56,142'],
-              ['4', 'Philip Chaney', 'Korea, South', 'Overland Park', '$38,735'],
-              ['5', 'Doris Greene', 'Malawi', 'Feldkirchen in Kärnten', '$63,542'],
-              ['6', 'Mason Porter', 'Chile', 'Gloucester', '$78,615']
-          ]
-      };
-      this.tableData2 = {
-          headerRow: [ 'ID', 'Name',  'Salary', 'Country', 'City' ],
-          dataRows: [
-              ['1', 'Dakota Rice','$36,738', 'Niger', 'Oud-Turnhout' ],
-              ['2', 'Minerva Hooper', '$23,789', 'Curaçao', 'Sinaai-Waas'],
-              ['3', 'Sage Rodriguez', '$56,142', 'Netherlands', 'Baileux' ],
-              ['4', 'Philip Chaney', '$38,735', 'Korea, South', 'Overland Park' ],
-              ['5', 'Doris Greene', '$63,542', 'Malawi', 'Feldkirchen in Kärnten', ],
-              ['6', 'Mason Porter', '$78,615', 'Chile', 'Gloucester' ]
+              ['1', '80159100', 'HLORL', 'Initial', 'ME1','UNDERWRITER STAMP','','Print Preview','0.00','69001357'],
+              ['2', '80159100', 'HLMED', 'Initial', 'ME1','NO BREAKFAST','','Print Preview','$0.00','69001357'],
+              ['3', '80159098', 'HLORL', 'Initial', 'ME1','UNDERWRITER STAMP','','Print Preview','$0.00','69001357'],
+              ['4', '80159098', 'HLMED', 'Initial', 'ME1','NO BREAKFAST','','Print Preview','$0.00','69001357'],
+              ['5', '80159095', 'HLORL', 'Initial', 'ME1','UNDERWRITER STAMP','','Print Preview','$0.00','69001357'],
+              ['6', '80159095', 'HLMED', 'Initial', 'ME1','NO BREAKFAST','','Print Preview','$0.00','69001357']
           ]
       };
   }
